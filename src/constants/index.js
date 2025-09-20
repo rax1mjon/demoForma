2770c88bdafcec44685a17f7f26e122758bf3d64
rax1mjon <tulaganovraximjon65@gmail.com>
2025-09-20 20:33:24 +0500

    delete request functions

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,3 @@
-export const ACCESSTOKEN = "ACCESS_TOKEN";
-export const REFRESHTOKEN = "REFRESH_TOKEN";
 export const ROLE = "ROLE";
 
 export const ENDPOINT = "https://digitalofficedatacenter.uz";
